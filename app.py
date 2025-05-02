6c38b80c55149273627e756fa81e9ad3731cff53
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-05-02 14:35:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return """Hello, World! from artifact registry  {
-  "db_user": "vamsi",
-  "db_pass": "securepassword123",
-  "api_key": "your-api-key-value"
-} """
+    return "hello browser this code is running from github to cloud run"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
